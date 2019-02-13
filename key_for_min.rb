4c6b63e90b9dde68974fbb3b59b9f3473093e00b
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 20:04:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ require "pry"
 def key_for_min_value(name_hash)
   old_hash = name_hash #Should be {item => number}
   name_hash.each do |item, number| #item is the key, number is the value
-    if number < old_hash[item]
       binding.pry
+    if number < old_hash[item]
       min_item = item
     end
   end
