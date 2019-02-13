1849d54047da67888b9f03323d0e63bfb0d7111a
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 19:32:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_item_and_num = name_hash
+  min_item = name_hash
   name_hash.collect do |item, number|
     if number < old_hash[number]
-      min_item_and_num = {item => number}
+      min_item = item
     end
+    old_hash = {item, number}
   end
-  min_item_and_num
+  min_item
 end
