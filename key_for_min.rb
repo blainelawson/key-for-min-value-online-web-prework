03558b18ae67c44cebb18637b0764334062e3652
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 20:07:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   old_hash = name_hash #Should be {item => number}
   name_hash.each do |item, number| #item is the key, number is the value
     if number <= old_hash[item]
+      bingding.pry
       min_item = item
     end
   end
