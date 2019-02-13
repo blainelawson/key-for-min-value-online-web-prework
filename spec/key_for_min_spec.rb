ee7b67d69d21a76cdf97b09fcfd7d04ef6f978cc
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 20:29:45 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -40,9 +40,9 @@ describe "smallest hash value" do
     key_for_min_value(hash)
   end
 
-# it "returns the key of the smallest hash value" do
-#    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-#  end
+it "returns the key of the smallest hash value" do
+    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
+  end
 
   it "returns the key of the smallest hash value example 2" do
     expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
