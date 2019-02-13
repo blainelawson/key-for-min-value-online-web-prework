5678170ac5175993e77a73d8ea201c9592b6be42
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 19:57:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   old_hash = name_hash #Should be {item => number}
   min_item = {}
-  name_hash.collect do |item, number| #item is the key, number is the value
+  name_hash.each do |item, number| #item is the key, number is the value
     old_number = old_hash[item]
     if number < old_number
       min_item = item
